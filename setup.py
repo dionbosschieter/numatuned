957eae4826b018fecaa1c6b2cbd905342a550afc
Dion Bosschieter <dbosschieter@debbuilder1.transip.us>
2017-09-27 15:11:45 +0200

    Including service file

--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
+import os
+from setuptools.command.install import install
 from setuptools import setup
 
+class CustomerInstaller(install):
+
+  CREATE_DIRS = [
+      os.path.join('/lib', 'systemd', 'system'),
+  ]
+
+  def run(self):
+    """
+    Installs and then copies the service file to the systemd directory
+    """
+    install.run(self)
+    print("Creating needed directories")
+    for directory in self.CREATE_DIRS:
+        if not os.path.exists(directory):
+            try:
+                os.makedirs(directory)
+            except OSError as exc:
+                if exc.errno == 13:
+                    print("WARNING! Failed to create directory '{}'. This "
+                          "might cause problems.".format(directory))
+                else:
+                    raise
+
 setup(name='numatuned',
-      version='0.1',
+      version='0.2',
       description='Numad replacement for qemu/libvirtd virtual machines that uses virsh numatune --nodeset to bind a virtual machine to a numa zone',
       url='http://github.com/dionbosschieter/numatuned',
       author='Dion Bosschieter',
       author_email='dbosschieter@transip.nl',
       license='MIT',
       packages=['numatuned', 'numatuned.provisioning', 'numatuned.provisioning.rules'],
+      package_data={
+          'numatuned': ['bin/numatuned.service'],
+      },
+      zip_safe=False,
       scripts=['bin/numatuned'],
-      zip_safe=False)
+      data_files=[('/lib/systemd/system', ['bin/numatuned.service'])],
+      cmdclass={'install': CustomerInstaller},
+)
